e34db241122f45dcb566f216c19b77969c7fd762
Zamizmi <sami.k.lindqvist@gmail.com>
2017-02-13 08:50:04 +0200

    fixed test layout

--- spec/features/ratings_page_spec.rb
+++ spec/features/ratings_page_spec.rb
@@ -44,16 +44,17 @@ describe "Rating" do
   end
 
   it "deleted by user are also deleted from the Database" do
-    FactoryGirl.create :rating, beer_id:1, user_id:1
-    FactoryGirl.create :rating, score:8, beer_id:2, user_id:1
-    matti = User.create username:"Matti", password:"Matti1", password_confirmation:"Matti1"
-    FactoryGirl.create :rating2, beer_id:3, user_id:2
+    matti = User.create! username:"Matti", password:"Matti1", password_confirmation:"Matti1"
+    kalle = User.create! username:"Kalle", password:"Kalle1", password_confirmation:"Kalle1"
     sign_in(username:"Matti", password:"Matti1")
-    visit user_path(matti)
-    save_and_open_page
+    FactoryGirl.create :rating, beer_id:1, user_id:matti.id
+    FactoryGirl.create :rating, score:8, beer_id:2, user_id:matti.id
+    FactoryGirl.create :rating2, beer_id:3, user_id:kalle.id
+    visit user_path(matti.id)
     page.find('tr', :text => "Karhu: Koff").click_link('Delete')
+    save_and_open_page
 
-    expect(page).to have_content("Has made 2 ratings, with the average of 9.0")
+    expect(page).to have_content("Has made 1 rating , with the average of 10.0")
     expect(Rating.count).to eq(2)
   end
 end
