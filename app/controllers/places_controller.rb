57fa8e558e89071d9eaad3d2f2f370b4efa211c7
Zamizmi <sami.k.lindqvist@gmail.com>
2017-02-16 18:11:30 +0200

    travis fixed

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,9 @@
 class PlacesController < ApplicationController
+
   def index
+  end
+
+  def show
   end
 
   def search
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :breweries
   resources :ratings, only: [:index, :new, :create, :destroy]
   resource :session, only: [:new, :create, :destroy]
+  resources :places, only:[:index, :show]
 
   root 'breweries#index'
   get 'signup', to: 'users#new'
